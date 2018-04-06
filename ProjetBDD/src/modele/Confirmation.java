0e3ed726752ca8beb74ef6b4fccc349e99252080
thibautM <34159900+masselit@users.noreply.github.com>
2018-04-06 13:54:39 +0200

    première partie de Confirmation
    refactoring de InsertionScanner
    redéfinition type pour la dateString (String en Date)

--- ProjetBDD/src/modele/Confirmation.java
+++ ProjetBDD/src/modele/Confirmation.java
@@ -1,6 +1,15 @@
 package modele;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import modele.type.Seminaire;
+import requete.Requetes;
 
 /**
  * 
@@ -10,15 +19,54 @@ import java.sql.Connection;
  */
 public class Confirmation extends ActionSeminaire {
 
+	private Calendar cal = Calendar.getInstance();
+	
 	@Override
 	public void action(Connection conn) {
 		// Code de la Confirmation
-		/*
-		Confirmer un séminaire 1 semaine avant sa date planifiée :
-		• annulation si le nombre d'inscriptions est insuffisant
-		• prévenir le prestataire (soit annulation, soit confirmation avec le nombre de personnes)
-		• bilan budgétaire si le séminaire a lieu
-		*/
+
+		//Confirmer un séminaire 1 semaine avant sa date planifiée :
+		try {
+			cal.add(Calendar.DATE, 7);
+			List<Seminaire> seminaires = new ArrayList<>();
+		
+			seminaires.addAll(Requetes.selectLesSeminaire(conn));
+			
+			annulationDeSeminaire(conn,seminaires);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	
+	/**
+	 * Mette les séminaires à annulé dans l'état ANN
+	 * et les supprime de la liste
+	 * @param conn
+	 * @param seminaires
+	 * @throws SQLException
+	 */
+	private void annulationDeSeminaire(Connection conn, List<Seminaire> seminaires) throws SQLException{
+		for(Seminaire semi : seminaires){
+			Requetes.selectNbInscrit(conn,semi);
+			
+			//• annulation si le nombre d'inscriptions est insuffisant
+			if(Math.round(semi.getNombrePlace()/2) <= semi.getNombrePersonneActuelle() 
+					&& cal.getTime().compareTo(semi.getDate()) == -1){
+				semi.setEtatSemi("ANN");
+				System.out.println("Le nombre d'inscriptions est insuffisant pour :"+ semi.getLibelle() );
+				seminaires.remove(semi);
+			}
+		}
+	}
+		//• prévenir le prestataire (soit annulation, soit confirmation avec le nombre de personnes)
+		//for(Seminaire semi : seminaires){
+		
+		
+		//• bilan budgétaire si le séminaire a lieu
+		
+		
 		
         //nombre d'inscriptions est insuffisant
 		/*
@@ -35,6 +83,5 @@ public class Confirmation extends ActionSeminaire {
 			//prix
 		}
 		 */
-	}
 	
 }
